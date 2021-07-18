ef69d7f768e9da81fd58c114e3de729fcd21b383
Katharine C Humble <katharinechumble@gmail.com>
2021-07-18 14:06:08 -0400

    added time blocks and ability to edit tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,28 @@
-function userInput() {
-    // get html content corresponding to user's click
+console.log("GUTEN MORGEN HERR PTERODACTYL");
 
+var tasks = {};
+
+function userInput(taskInput, taskText) {
+    // get html content corresponding to user's click
+    $(".new-task").on("click", "p", function() {
+        var task = $(this)
+        .html()
+        .trim();
+    console.log(this);
     // make editable middle segment
-    
+        var taskInput = $("<textarea>")
+            .addClass("new-task")
+            .val(task);
+        var taskP = $("<p>")
+            .addClass("m-1")
+            .text(taskText);
+        $(this).replaceWith(taskInput);
+        taskInput.trigger("focus");
+    }); 
+
 }
+
+// save tasks to local storage
+
+
+userInput();
